169e675f0f51a6db47694f1127494a9f9d151098
jhvargas3112 <jhvargas@ucm.es>
2019-03-16 00:14:36 +0100

    My first commit

---
+++ src/arrays/Problem123.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Problem123 {
+
+  static java.util.Scanner in;
+
+  public static boolean resolveCase() {
+
+    return true;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    in = new java.util.Scanner(System.in);
+    while (resolveCase())
+      ;
+  }
+
+}
